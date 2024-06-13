69a2aea9fc89cf21f5840ae87d7c455a9eaeb966
jinya1013 <gab102manda100@gmail.com>
2024-06-14 00:01:59 +0900

    openacc added

--- 15_final/submission/05_cavity_openacc.cpp
+++ 15_final/submission/05_cavity_openacc.cpp
@@ -5,209 +5,109 @@
 #include <openacc.h>
 using namespace std;
 
-void set_linspace(double* result, double start, double end, int num) {
-    if (num <= 0) {
-        return;
-    }
-    if (num == 1) {
-        result[0] = start;
-        return;
-    }
-
-    double step = (end - start) / (num - 1);
-    for (int i = 0; i < num; ++i) {
-        result[i] = start + step * i;
-    }
-}
-
-void set_zeros2d(double** result, int rows, int cols) {
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            result[i][j] = 0;
-        }
-    }
-}
-
-double** allocate_2d(int rows, int cols) {
-    double** array = (double**)malloc(rows * sizeof(double*));
-    for (int i = 0; i < rows; ++i) {
-        array[i] = (double*)malloc(cols * sizeof(double));
-    }
-    return array;
-}
-void print_2d(double** array, int rows, int cols) {
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            std::cout << array[i][j] << " ";
-        }
-        std::cout << std::endl;
-
-        if (i == 1) {
-            break;
-        }
-    }
-}
-void print_1d(double* array, int size) {
+void print_1d_array(double* array, int size) {
     for (int i = 0; i < size; ++i) {
         std::cout << array[i] << " ";
     }
     std::cout << std::endl;
 }
 
-void copy_2d(double** src, double** dest, int rows, int cols) {
-    for (int i = 0; i < rows; ++i) {
-        std::copy(src[i], src[i] + cols, dest[i]);
-    }
-}
-
-void free_2d(double** array, int rows) {
-    for (int i = 0; i < rows; ++i) {
-        free(array[i]);
-    }
-    free(array);
-}
-
 int main() {
-    int nx = 40, ny = 40, nt = 500, nit = 50;
+    int nx = 41, ny = 41, nt = 500, nit = 50;
     double dx = 2.0 / (nx - 1), dy = 2.0 / (ny - 1), dt = 0.01;
     double rho = 1.0, nu = 0.02;
 
-    double x[nx], y[ny];
+    double u[ny*nx], v[ny*nx], p[ny*nx], b[ny*nx];
+    double un[ny*nx], vn[ny*nx], pn[ny*nx];
 
-    set_linspace(x, 0, 2, nx);
-    set_linspace(y, 0, 2, ny);
-
-    double** u = allocate_2d(ny, nx);
-    double** v = allocate_2d(ny, nx);
-    double** p = allocate_2d(ny, nx);
-    double** b = allocate_2d(ny, nx);
-
-    double** un = allocate_2d(ny, nx);
-    double** vn = allocate_2d(ny, nx);
-    double** pn = allocate_2d(ny, nx);
-
-    set_zeros2d(u, ny, nx);
-    set_zeros2d(v, ny, nx);
-    set_zeros2d(p, ny, nx);
-    set_zeros2d(b, ny, nx);
-
-    set_zeros2d(un, ny, nx);
-    set_zeros2d(vn, ny, nx);
-    set_zeros2d(pn, ny, nx);
+    for (int idx = 0; idx < ny * nx; ++idx) {
+        u[idx] = v[idx] = p[idx] = b[idx] = un[idx] = vn[idx] = pn[idx] = 0;
+    }
 
     auto tic = chrono::steady_clock::now();
 
     for (int n = 0; n < nt; ++n) {
-        if (n % 2 == 0) {
-            #pragma acc kernels 
-            for (int idx = 0; idx < (ny - 2) * (nx - 2); ++idx) {
-                int j = 1 + idx / (nx - 2);
-                int i = 1 + idx % (nx - 2);
-                b[j][i] = rho * (1 / dt *
-                    ((u[j][i+1] - u[j][i-1]) / (2 * dx) + (v[j+1][i] - v[j-1][i]) / (2 * dy)) -
-                    ((u[j][i+1] - u[j][i-1]) / (2 * dx)) * ((u[j][i+1] - u[j][i-1]) / (2 * dx)) - 2 * ((u[j+1][i] - u[j-1][i]) / (2 * dy) *
-                        (v[j][i+1] - v[j][i-1]) / (2 * dx)) - ((v[j+1][i] - v[j-1][i]) / (2 * dy)) * ((v[j+1][i] - v[j-1][i]) / (2 * dy)));
-            }
-        } else {
-            #pragma acc kernels
-            for (int idx = 0; idx < (ny - 2) * (nx - 2); ++idx) {
-                int j = 1 + idx / (nx - 2);
-                int i = 1 + idx % (nx - 2);
-                b[j][i] = rho * (1 / dt *
-                    ((un[j][i+1] - un[j][i-1]) / (2 * dx) + (vn[j+1][i] - vn[j-1][i]) / (2 * dy)) -
-                    ((un[j][i+1] - un[j][i-1]) / (2 * dx)) * ((un[j][i+1] - un[j][i-1]) / (2 * dx)) - 2 * ((un[j+1][i] - un[j-1][i]) / (2 * dy) *
-                        (vn[j][i+1] - vn[j][i-1]) / (2 * dx)) - ((vn[j+1][i] - vn[j-1][i]) / (2 * dy)) * ((vn[j+1][i] - vn[j-1][i]) / (2 * dy)));
+        # pragma acc kernels
+        for (int j = 1; j < ny - 1; ++j) {
+            for (int i = 1; i < nx - 1; ++i) {
+                int idx = j * nx + i;
+                int idx_ip1 = j * nx + (i + 1);
+                int idx_im1 = j * nx + (i - 1);
+                int idx_jp1 = (j + 1) * nx + i;
+                int idx_jm1 = (j - 1) * nx + i;
+
+                b[idx] = rho * (1 / dt *
+                    ((u[idx_ip1] - u[idx_im1]) / (2 * dx) + (v[idx_jp1] - v[idx_jm1]) / (2 * dy)) -
+                    ((u[idx_ip1] - u[idx_im1]) / (2 * dx)) * ((u[idx_ip1] - u[idx_im1]) / (2 * dx)) - 
+                    2 * ((u[idx_jp1] - u[idx_jm1]) / (2 * dy) * (v[idx_ip1] - v[idx_im1]) / (2 * dx)) - 
+                    ((v[idx_jp1] - v[idx_jm1]) / (2 * dy)) * ((v[idx_jp1] - v[idx_jm1]) / (2 * dy)));
             }
         }
-        
         for (int it = 0; it < nit; ++it) {
-            if (it % 2 != 0) {
-                # pragma acc kernels 
-                for (int idx = 0; idx < (ny - 2) * (nx - 2); ++idx) {
-                    int j = 1 + idx / (nx - 2);
-                    int i = 1 + idx % (nx - 2);
-                    p[j][i] = (dy * dy * (pn[j][i+1] + pn[j][i-1]) + 
-                        dx * dx * (pn[j+1][i] + pn[j-1][i]) -
-                        b[j][i] * dx * dx * dy * dy) / (2 * (dx * dx + dy * dy));
+            std::copy(p, p+ny*nx, pn);
+            # pragma acc kernels
+            for (int j = 1; j < ny - 1; ++j) {
+                for (int i = 1; i < nx - 1; ++i) {
+                    int idx = j * nx + i;
+                    int idx_ip1 = j * nx + (i + 1);
+                    int idx_im1 = j * nx + (i - 1);
+                    int idx_jp1 = (j + 1) * nx + i;
+                    int idx_jm1 = (j - 1) * nx + i;
+                    p[idx] = (dy * dy * (pn[idx_ip1] + pn[idx_im1]) + 
+                        dx * dx * (pn[idx_jp1] + pn[idx_jm1]) -
+                        b[idx] * dx * dx * dy * dy) / (2 * (dx * dx + dy * dy));
                 }
-                for (int j = 0; j < ny; ++j) {
-                    p[j][0] = p[j][1]; 
-                    p[j][nx - 1] = p[j][nx - 2];
-                }
-                for (int i = 0; i < nx; ++i) {
-                    p[0][i] = p[1][i]; 
-                    p[ny - 1][i] = 0; 
-                }
-            } else {
-                # pragma acc kernels 
-                for (int idx = 0; idx < (ny - 2) * (nx - 2); ++idx) {
-                    int j = 1 + idx / (nx - 2);
-                    int i = 1 + idx % (nx - 2);
-                    pn[j][i] = (dy * dy * (p[j][i+1] + p[j][i-1]) + 
-                        dx * dx * (p[j+1][i] + p[j-1][i]) -
-                        b[j][i] * dx * dx * dy * dy) / (2 * (dx * dx + dy * dy));
-                }
-                for (int j = 0; j < ny; ++j) {
-                    pn[j][0] = pn[j][1]; 
-                    pn[j][nx - 1] = pn[j][nx - 2];
-                }
-                for (int i = 0; i < nx; ++i) {
-                    pn[0][i] = pn[1][i]; 
-                    pn[ny - 1][i] = 0; 
-                }
+            }
+            for (int j = 0; j < ny; ++j) {
+                p[j * ny + nx- 1] = p[j * ny + nx - 2];
+            }
+            for (int i = 0; i < nx; ++i) {
+                p[i] = p[ny + i]; 
+            }
+            for (int j = 0; j < ny; ++j) {
+                p[j * ny] = p[j * ny + 1]; 
+            }
+            for (int i = 0; i < nx; ++i) {
+                p[(ny - 1) * ny + i] = 0; 
             }
         }
-        
-        if (n % 2 != 0) {
-            # pragma acc kernels 
-            for (int idx = 0; idx < (ny - 2) * (nx - 2); ++idx) {
-                int j = 1 + idx / (nx - 2);
-                int i = 1 + idx % (nx - 2);
-                u[j][i] = un[j][i] - un[j][i] * dt / dx * (un[j][i] - un[j][i-1])
-                        - un[j][i] * dt / dy * (un[j][i] - un[j-1][i])
-                        - dt / (2 * rho * dx) * (p[j][i+1] - p[j][i-1])
-                        + nu * dt / (dx*dx) * (un[j][i+1] - 2 * un[j][i] + un[j][i-1])
-                        + nu * dt / (dy*dy) * (un[j+1][i] - 2 * un[j][i] + un[j-1][i]);
-                v[j][i] = vn[j][i] - vn[j][i] * dt / dx * (vn[j][i] - vn[j][i-1])
-                                - vn[j][i] * dt / dy * (vn[j][i] - vn[j-1][i])
-                                - dt / (2 * rho * dx) * (p[j+1][i] - p[j-1][i])
-                                + nu * dt / (dx*dx) * (vn[j][i+1] - 2 * vn[j][i] + vn[j][i-1])
-                                + nu * dt / (dy*dy) * (vn[j+1][i] - 2 * vn[j][i] + vn[j-1][i]);
+        std::copy(u, u+ny*nx, un);
+        std::copy(v, v+ny*nx, vn);
+        # pragma acc kernels
+        for (int j = 1; j < ny - 1; ++j) {
+            for (int i = 1; i < nx - 1; ++i) {
+                int idx = j * nx + i;
+                int idx_ip1 = j * nx + (i + 1);
+                int idx_im1 = j * nx + (i - 1);
+                int idx_jp1 = (j + 1) * nx + i;
+                int idx_jm1 = (j - 1) * nx + i;
+                u[idx] = un[idx] - un[idx] * dt / dx * (un[idx] - un[idx_im1])
+                        - un[idx] * dt / dy * (un[idx] - un[idx_jm1])
+                        - dt / (2 * rho * dx) * (p[idx_ip1] - p[idx_im1])
+                        + nu * dt / (dx*dx) * (un[idx_ip1] - 2 * un[idx] + un[idx_im1])
+                        + nu * dt / (dy*dy) * (un[idx_jp1] - 2 * un[idx] + un[idx_jm1]);
+                v[idx] = vn[idx] - vn[idx] * dt / dx * (vn[idx] - vn[idx_im1])
+                                - vn[idx] * dt / dy * (vn[idx] - vn[idx_jm1])
+                                - dt / (2 * rho * dx) * (p[idx_jp1] - p[idx_jm1])
+                                + nu * dt / (dx*dx) * (vn[idx_ip1] - 2 * vn[idx] + vn[idx_im1])
+                                + nu * dt / (dy*dy) * (vn[idx_jp1] - 2 * vn[idx] + vn[idx_jm1]);
             }
-            for (int i = 0; i < nx; ++i) {
-                u[0][i] = v[0][i] = 0;
-                u[ny - 1][i] = 1;
-                v[ny - 1][i] = 0;
-            }
-            for (int j = 0; j < ny; ++j) {
-                u[j][0] = v[j][0] = 0;
-                u[j][nx - 1] = v[j][nx - 1] = 0;
-            }
-        } else {
-            # pragma acc kernels 
-            for (int idx = 0; idx < (ny - 2) * (nx - 2); ++idx) {
-                int j = 1 + idx / (nx - 2);
-                int i = 1 + idx % (nx - 2);
-                un[j][i] = u[j][i] - u[j][i] * dt / dx * (u[j][i] - u[j][i-1])
-                        - u[j][i] * dt / dy * (u[j][i] - u[j-1][i])
-                        - dt / (2 * rho * dx) * (p[j][i+1] - p[j][i-1])
-                        + nu * dt / (dx*dx) * (u[j][i+1] - 2 * u[j][i] + u[j][i-1])
-                        + nu * dt / (dy*dy) * (u[j+1][i] - 2 * u[j][i] + u[j-1][i]);
-                vn[j][i] = v[j][i] - v[j][i] * dt / dx * (v[j][i] - v[j][i-1])
-                                - v[j][i] * dt / dy * (v[j][i] - v[j-1][i])
-                                - dt / (2 * rho * dx) * (p[j+1][i] - p[j-1][i])
-                                + nu * dt / (dx*dx) * (v[j][i+1] - 2 * v[j][i] + v[j][i-1])
-                                + nu * dt / (dy*dy) * (v[j+1][i] - 2 * v[j][i] + v[j-1][i]);
-            }
-            for (int i = 0; i < nx; ++i) {
-                un[0][i] = vn[0][i] = 0;
-                un[ny - 1][i] = 1;
-                vn[ny - 1][i] = 0;
-            }
-            for (int j = 0; j < ny; ++j) {
-                un[j][0] = vn[j][0] = 0;
-                un[j][nx - 1] = vn[j][nx - 1] = 0;
-            }
+        }
+        for (int i = 0; i < nx; ++i) {
+            u[i] = 0;
+            u[(ny - 1) * ny + i] = 1;
+        }
+        for (int j = 0; j < ny; ++j) {
+            u[j * ny] = 0;
+            u[j * ny + nx - 1] = 0;
+        }
+        for (int i = 0; i < nx; ++i) {
+            v[i] = 0;
+            v[(ny - 1) * ny + i] = 0;
+        }
+        for (int j = 0; j < ny; ++j) {
+            v[j * ny] = 0;
+            v[j * ny + nx - 1] = 0;
         }
     }
 
@@ -215,19 +115,11 @@ int main() {
     double time = chrono::duration<double>(toc - tic).count();
 
     std::cout << "u" << std::endl;
-    print_2d(u, ny, nx);
+    print_1d_array(u, 2*ny);
     std::cout << "v" << std::endl;
-    print_2d(v, ny, nx);
+    print_1d_array(v, 2*ny);
 
     std::cout << "time: " << time << "s" << std::endl;
-
-    free_2d(u, ny);
-    free_2d(v, ny);
-    free_2d(p, ny);
-    free_2d(b, ny);
-    free_2d(un, ny);
-    free_2d(vn, ny);
-    free_2d(pn, ny);
 
     return 0;
 }
